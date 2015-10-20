e2efedaf2b0c9866963740354cab8c4e0c37b4c7
Ighor Martins <ighor.martins@gmail.com>
2015-10-20 21:33:20 +0100

    Refactored layout and design

--- app/items/items.add.controller.js
+++ app/items/items.add.controller.js
@@ -16,6 +16,8 @@
 
 
         vm.submit = submit;
+
+
         activate();
         
         function activate() {
@@ -25,8 +27,13 @@
         }
 
         function submit(){
-            if(vm.newItem !== undefined && vm.newItem.name !== undefined && vm.newItem.container !== undefined){
-                vm.newItem.$save();
+            if(vm.newItem !== undefined && vm.newItem.name !== undefined && vm.newItem.container !== undefined && vm.newItem.image !== undefined){
+                vm.newItem.$save(function(item, putResponseHeaders) {
+                    $mdToast.show($mdToast.simple().content('Nice, o item ' + item.name + ' foi adicionado!'));
+                    var container = vm.newItem.container;
+                    vm.newItem = new ItemsFactory();
+                    vm.newItem.container = container;
+                });
             }else{
                 $mdToast.show($mdToast.simple().content('Preenche tudo nabo nabo!'));
             }
--- app/items/items.list.controller.js
+++ app/items/items.list.controller.js
@@ -1,27 +1,33 @@
-(function() {
+(function () {
     'use strict';
-    
+
     angular
         .module('app')
         .controller('ItemsListController', Controller);
-    
-    Controller.$inject = ['ItemsService'];
-    
+
+    Controller.$inject = ['ItemsService', '$state', '$scope'];
+
     /* @ngInject */
-    function Controller(ItemsService) {
+    function Controller(ItemsService, $state, $scope) {
         var vm = this;
         vm.title = 'Controller';
-        
+
         activate();
-        
-        ////////////////
-        
+
         function activate() {
             ItemsService.all().$promise.then(
-                function(data){
-                    console.log(data);
+                function (data) {
                     vm.items = data.results;
-            });
+                });
         }
+
+        $scope.$watch(function () {
+            return vm.searchText;
+        }, function (current, original) {
+
+            if(angular.lowercase(current) === 'i know the secret and i want to add an object'){
+                $state.go('items_add');
+            }
+        });
     }
 })();
