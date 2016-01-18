48f01c0c3ffe8fa739067ecb453e76bb385da699
Ighor Martins <ighor.martins@gmail.com>
2016-01-18 22:39:35 +0000

    Fixed JSCS warnings for items module

--- app/items/items.factory.js
+++ app/items/items.factory.js
@@ -1,14 +1,14 @@
 (function() {
     'use strict';
-    
+
     angular
         .module('app.items')
         .factory('ItemsFactory', factory);
-    
+
     factory.$inject = ['$resource', 'AppSettings'];
-    
+
     /* @ngInject */
     function factory($resource, AppSettings) {
-        return $resource(AppSettings.apiUrl + '/items/:id', {}, { query: { } });
+        return $resource(AppSettings.apiUrl + '/items/:id', {}, {query: {}});
     }
 })();
--- app/items/items.service.js
+++ app/items/items.service.js
@@ -1,27 +1,26 @@
-
 (function() {
     'use strict';
-    
+
     angular
         .module('app.items')
         .service('ItemsService', Service);
-    
+
     Service.$inject = ['ItemsFactory', 'ContainersService', '$q'];
-    
+
     /* @ngInject */
     function Service(ItemsFactory, ContainersService, $q) {
         this.all = all;
 
         function all() {
             var items;
-            return ItemsFactory.query(function(data){
+            return ItemsFactory.query(function(data) {
                 items = data.results;
                 var requests = [];
                 // for each item in the items array make a request to get the container data
-                angular.forEach(items, function(item){
+                angular.forEach(items, function(item) {
                     // get related resource data using container id as foreign key
                     var request = ContainersService.get(item.container);
-                    request.then(function(container){
+                    request.then(function(container) {
                         item.container = container;
                     });
                     // add the request to the requests array
