d295540e86cda1f870387b53ddf29da3232fac0e
Ighor Martins <ighor.martins@gmail.com>
2016-09-28 14:41:05 +0100

    Fixes app not redirecting to login if user token was expired

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,27 +1,39 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('app')
-    .run(runBlock);
+    angular
+        .module('app')
+        .run(runBlock);
 
-  /** @ngInject */
-  function runBlock($rootScope, $state, AccountsTable) {
-    $rootScope.$state = $state;
-    $rootScope.mainStateChangeStart = $rootScope.$on('$stateChangeStart', function (event, toState) {
+    /** @ngInject */
+    function runBlock($rootScope, $state, AccountsTable) {
+        $rootScope.$state = $state;
+        $rootScope.loginChecked = false;
+        $rootScope.mainStateChangeStart = $rootScope.$on('$stateChangeStart', function (event, toState) {
 
-      var user = localStorage.getItem('member');
-      if (user !== 'undefined' && user !== null) {
-        user = angular.fromJson(user);
+            var user = localStorage.getItem('member');
+            if (user !== 'undefined' && user !== null) {
+                user = angular.fromJson(user);
 
-        $rootScope.me = AccountsTable.$build(user);
-        $rootScope.me.$pk = user.id;
-        if (toState.name === 'auth') {
-          event.preventDefault();
-          //$state.go('itemsList');
-        }
-      }
-    });
-  }
+
+                if (!$rootScope.loginChecked) {
+                    $rootScope.me = AccountsTable.$find('me').$then(function () {
+                    }, function () {
+                        $state.go('logout');
+                    });
+                    $rootScope.loginChecked = true;
+                } else {
+                    $rootScope.me = AccountsTable.$build(user);
+                }
+                $rootScope.me.$pk = user.id;
+
+
+                if (toState.name === 'auth') {
+                    event.preventDefault();
+                    //$state.go('itemsList');
+                }
+            }
+        });
+    }
 
 })();
